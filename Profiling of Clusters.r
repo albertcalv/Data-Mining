17c44c9da81c9d1c17471aeb5730242477590be1
albertcalv <albertcalv@gmail.com>
2015-10-24 00:43:07 +0200

    Profiling Clusters

---
+++ Profiling of Clusters.r
@@ -0,0 +1,12 @@
+#Principal Component Analysis
+#further reading: http://gastonsanchez.com/blog/how-to/2012/06/17/PCA-in-R.html
+
+setwd("/home/albert")
+dd<-read.csv("NBA.csv",header=T, sep=",")
+
+attach(dd)
+names(dd)
+summary(dd)
+
+
+var_num <- data.frame(away_score, home_score, converted_x, converted_y)
